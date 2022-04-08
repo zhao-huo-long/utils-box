57c0058caf488f7b4e2a8f2a897d88af75a00476
zhao-huo-long <lijiuyi1995@outlook.com>
2022-04-08 10:33:32 +0800

    wip

--- src/Function/Debounce/index.ts
+++ src/Function/Debounce/index.ts
@@ -1,17 +1,17 @@
-type Func = (...args: any[]) => any
+type Func = (...args: any[]) => any;
 
 /**
  * debounce
-*/
-const debounce = function<T extends Func>(cb: T, delayTime = 0 ) {
-  let preTimeid: number | undefined = undefined
-  return function(...args: Parameters<T>): void{
-    if(preTimeid) clearTimeout(preTimeid)
-    preTimeid = setTimeout(()=> {
-      preTimeid = undefined
-      cb.call(this, ...args)
-    }, delayTime)
-  }
-}
+ */
+const debounce = function <T extends Func>(cb: T, delayTime = 0) {
+  let preTimeid: number | undefined = undefined;
+  return function (...args: Parameters<T>): void {
+    clearTimeout(preTimeid);
+    preTimeid = setTimeout(() => {
+      preTimeid = undefined;
+      cb.call(this, ...args);
+    }, delayTime);
+  };
+};
 
-export default debounce
+export default debounce;
